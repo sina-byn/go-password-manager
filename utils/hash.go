fdf4914340ba0d2a16982e56b51af5ce1047e4f3
sina-byn <sina.sina.bayan@protonmail.com>
2024-07-03 18:18:43 +0330

    HashPassword and Login utils added

---
+++ utils/hash.go
@@ -0,0 +1,52 @@
+package utils
+
+import (
+	"fmt"
+	"log"
+
+	"golang.org/x/crypto/bcrypt"
+
+	"github.com/sina-byn/go-password-manager/db"
+)
+
+func HashPassword(password string) string {
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), 12)
+
+	if err != nil {
+		log.Fatalf("Failed to hash password: %v", err)
+	}
+
+	return string(hashedPassword)
+}
+
+func Login() int {
+	var id int
+	var hashedPassword string
+	username := ""
+	password := ""
+
+	for len(username) < 1 {
+		fmt.Print("Username: ")
+		fmt.Scanln(&username)
+	}
+
+	for len(password) < 1 {
+		fmt.Print("Password: ")
+		fmt.Scanln(&password)
+	}
+
+	row := db.DB.QueryRow("SELECT * FROM users WHERE username = ?", username)
+	err := row.Scan(&id, &username, &hashedPassword)
+
+	if err != nil {
+		log.Fatalf("Invalid credentials")
+	}
+
+	err = bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(password))
+
+	if err != nil {
+		log.Fatalf("Invalid credentials")
+	}
+
+	return id
+}
